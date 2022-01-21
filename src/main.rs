97760d60a17689f1de9076163aedddf94fe8e279
Alex Black <wa7@sa34.cx>
2022-01-22 00:48:49 +0300

    Saturday already

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
 fn main() {
-    println!("Hello, world!");
+    dotenv::dotenv().ok();
+
+    // let token = dotenv::var("DO_TOKEN").unwrap();
+    // println!("{}", token);
+
+    // let rate_limit = dotenv::var("DO_RATE_LIMIT").unwrap().parse::<u16>().unwrap();
+
+    // for i in 1..=20 {
+    //     let interval = calculate_polling_interval(rate_limit, i);
+    //     println!("For {} instances polling can be every {} secs", i, interval);
+    // } 
+
+    // let rate_limit: u16 = 5000;
+    // let calc = rate_limit / 60 / 4;
+    // println!("{}", calc);
+}
+
+// Polling every n secs
+fn calculate_polling_interval(rate_limit: u16, apps_num: u16) -> u16 {
+    60 / (rate_limit  / 60 / apps_num) + 5
 }
